bee2ee33add7df71ac1e9647bbf9f4ca69892953
andjela <andjelamarinovic1@gmail.com>
2025-01-20 16:06:09 +0100

    Implementacija početne stranice,izbornik

--- components/Routes.js
+++ components/Routes.js
@@ -1,6 +1,13 @@
+import Leaderboard from "./Leaderboard";
+import MyProfile from "./MyProfile";
+
 export const Routes = {
   LoggedOutView: "LoggedOutView",
   LoggedInView: "LoggedInView",
   RegistrationView: "RegistrationView",
   MainView: "MainView",
+  MyProfile:"MyProfile",
+  MemoriGame:"MemoriGame",
+  Igra2048:"Igra2048",
+  Leaderboard:"Leaderboard",
 };
